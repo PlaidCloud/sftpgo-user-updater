0fac5c5e15accd4a459b97672c53c37852293332
Kellen Kapper <kappersoftware@gmail.com>
2025-08-28 19:57:12 -0400

    maybe finally not dumb

--- sftpgo-user-updater.py
+++ sftpgo-user-updater.py
@@ -24,8 +24,7 @@ def get_new_user_data():
         content['filesystem']['provider'] = Provider.GCS.value
         content['filesystem']['gcsconfig']['bucket'] = os.environ['DEFAULT_USER_BUCKET']
         content['filesystem']['gcsconfig']['automatic_credentials'] = 1
-        content.update({'permissions': '/'})
-        content['permissions']['/'] = default_permissions
+        content['permissions'] = {'/' : default_permissions} 
         
     return jsonify(content)
     
