12fac9b4fce5b04bee5aa9eff3ce2cb5b1162732
Kellen Kapper <kappersoftware@gmail.com>
2025-08-28 14:23:46 -0400

    better format

--- sftpgo-user-updater.py
+++ sftpgo-user-updater.py
@@ -6,11 +6,12 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'])
 def get_new_user_data():
     content = request.get_json()
+    app.logger.debug('Formatted Body: %s', json.dumps(content))
     
 @app.before_request
 def log_request_info():
     app.logger.debug('Headers: %s', request.headers)
-    app.logger.debug('Body: %s', json.dumps(request.get_data()))
+    app.logger.debug('Body: %s', request.get_data())
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
