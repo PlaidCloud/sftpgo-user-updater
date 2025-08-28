03590a2fccc07b3f4c1701fe0364d7fdea80e5c4
Kellen Kapper <kappersoftware@gmail.com>
2025-08-28 16:58:45 -0400

    strings for values

--- sftpgo-user-updater.py
+++ sftpgo-user-updater.py
@@ -5,10 +5,10 @@ import os
 
 
 class Provider(Enum):
-    Local = 0
-    S3 = 1
-    GCS = 2
-    Azure = 3
+    Local = "0"
+    S3 = "1"
+    GCS = "2"
+    Azure = "3"
 
 app = Flask(__name__)
 
