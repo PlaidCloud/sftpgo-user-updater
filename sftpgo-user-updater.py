98e42ec00b5541b76c3ed5b113e83bfdd0f4725a
Kellen Kapper <kappersoftware@gmail.com>
2025-08-28 17:00:33 -0400

    and value

--- sftpgo-user-updater.py
+++ sftpgo-user-updater.py
@@ -5,10 +5,10 @@ import os
 
 
 class Provider(Enum):
-    Local = "0"
-    S3 = "1"
-    GCS = "2"
-    Azure = "3"
+    Local = 0
+    S3 = 1
+    GCS = 2
+    Azure = 3
 
 app = Flask(__name__)
 
@@ -19,7 +19,7 @@ def get_new_user_data():
     
     if(os.environ['STORAGE_PROVIDER'] == "GCS"):
         
-        content['filesystem']['provider'] = Provider.GCS
+        content['filesystem']['provider'] = Provider.GCS.value
         content['filesystem']['gcsconfig']['bucket'] = os.environ['DEFAULT_USER_BUCKET']
         content['filesystem']['gcsconfig']['automatic_credentials'] = 1
         
