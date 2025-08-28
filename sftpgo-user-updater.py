1d0680cab81716780daa23c2a391843beb5c4d31
Kellen Kapper <kappersoftware@gmail.com>
2025-08-28 14:06:15 -0400

    look at this json a bit easier

--- sftpgo-user-updater.py
+++ sftpgo-user-updater.py
@@ -1,15 +1,12 @@
 from flask import Flask, request
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello!, World!</p>"
-
-@app.before_request
-def log_request_info():
-    app.logger.debug('Headers: %s', request.headers)
-    app.logger.debug('Body: %s', request.get_data())
-
+@app.route("/", methods=['GET'])
+def get_new_user_data():
+    content = request.get_json()
+    print(json.dumps(content))
+    
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
