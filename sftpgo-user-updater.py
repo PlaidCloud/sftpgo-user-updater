ac8fe65e31f314eba1e710bcd82f526bf680038e
Kellen Kapper <kappersoftware@gmail.com>
2025-08-28 13:15:32 -0400

    debug logger for flask

--- sftpgo-user-updater.py
+++ sftpgo-user-updater.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello!, World!</p>"
 
+@app.before_request
+def log_request_info():
+    app.logger.debug('Headers: %s', request.headers)
+    app.logger.debug('Body: %s', request.get_data())
+
 if __name__ == "__main__":
     app.run(debug=True)
