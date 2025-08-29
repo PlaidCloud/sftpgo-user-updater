c38d17e8b4ca74c5aca7fac6f3fb513b998a15b0
Kellen Kapper <kappersoftware@gmail.com>
2025-08-29 10:21:34 -0400

    make implicit since it knows where to find files

--- sftpgo-user-updater.py
+++ sftpgo-user-updater.py
@@ -25,7 +25,7 @@ def get_new_user_data():
         content['home_dir'] = '/'
         content['filesystem']['provider'] = Provider.GCS.value
         content['filesystem']['gcsconfig']['bucket'] = os.environ['DEFAULT_USER_BUCKET']
-        content['filesystem']['gcsconfig']['automatic_credentials'] = 0
+        content['filesystem']['gcsconfig']['automatic_credentials'] = 1
         content['permissions'] = {'/' : default_permissions} 
         
     return jsonify(content)
