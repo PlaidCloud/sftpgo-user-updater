feed344c254de3e3b186929b24fa96ebddc2f1a3
Kellen Kapper <kappersoftware@gmail.com>
2025-08-28 20:01:00 -0400

    need to mark user as created

--- sftpgo-user-updater.py
+++ sftpgo-user-updater.py
@@ -21,6 +21,7 @@ def get_new_user_data():
         
         default_permissions = ['list', 'upload', 'overwrite', 'rename', 'copy']
         
+        content['status'] = 1
         content['filesystem']['provider'] = Provider.GCS.value
         content['filesystem']['gcsconfig']['bucket'] = os.environ['DEFAULT_USER_BUCKET']
         content['filesystem']['gcsconfig']['automatic_credentials'] = 1
